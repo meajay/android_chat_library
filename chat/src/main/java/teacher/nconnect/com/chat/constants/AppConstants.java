9a4af5460be292dc5b35b142f67dfe917203d7ba
ajay.chauhan1 <ajay.chauhan@gcf.education>
2018-08-07 13:07:04 +0530

    versiona and chat url change

--- chat/src/main/java/teacher/nconnect/com/chat/constants/AppConstants.java
+++ chat/src/main/java/teacher/nconnect/com/chat/constants/AppConstants.java
@@ -1,13 +1,11 @@
 package teacher.nconnect.com.chat.constants;
 
-import teacher.nconnect.com.chat.BuildConfig;
-
 /**
  * Created by Ajay on 25-06-2018.
  */
 public class AppConstants {
 
-    public static final String BASE_URL = BuildConfig.NCONNECT_BASE_URL;
+    public static final String BASE_URL = "http://chat.nkube.gcf.education";
     public static final String s3FolderName = "nconnect-chat";
     public static final String fileNamePrefix = "nchat-";
 
@@ -17,5 +15,5 @@ public class AppConstants {
     public static final String CHAT_TAG = "CHAT_LIBRARY";
 
     //Database version
-    public static final int VERSION = 2 ;
+    public static final int VERSION = 2;
 }
